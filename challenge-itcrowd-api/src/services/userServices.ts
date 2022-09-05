22f5493d2ebe1bebe46f0edeef9a58cdd3d44741
matiasjgelpi <matiasjgelpi@gmail.com>
2022-09-04 22:23:58 -0300

    token first implementation

--- challenge-itcrowd-api/src/services/userServices.ts
+++ challenge-itcrowd-api/src/services/userServices.ts
@@ -1,19 +1,37 @@
+import 'dotenv/config'
 import UserModel from '../database/models/userModel'
+import jwt from 'jsonwebtoken'
 
-const postUser = async (req: any, res: any): Promise<any> => {
-  try {
-    const { name, email, password } = req.body
-    const newUser = {
-      name,
-      email,
-      passwordHash: password
+// const secret = process.env.SECRET_KEY
+
+const postUser = (req: any, res: any): any => {
+  void (async () => {
+    try {
+      const { name, email, isAdministrator } = req.body
+
+      let user = await UserModel.findOne({ email: email })
+
+      if (user === null) {
+        const newUser = {
+          name,
+          email,
+          isAdministrator
+        }
+        user = await UserModel.create(newUser)
+      }
+
+      const token = jwt.sign(
+        { name: user.name, email: user.email, isAdministrator: user.isAdministrator },
+        'secret',
+        { expiresIn: '2h' }
+
+      )
+
+      return res.send({ user, token })
+    } catch (error: any) {
+      return res.status(400).send({ msg: error.toString() })
     }
-
-    const user = await UserModel.create(newUser)
-    return res.send(user)
-  } catch (error: any) {
-    return res.status(400).send({ msg: error.toString() })
-  }
+  })()
 }
 
 const userServices = {
